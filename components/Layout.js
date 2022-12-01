b4f28c9c01909f8beb0b00a73b7be65ff8a7b686
joshnevard <joshnevard@gmail.com>
2022-12-01 13:57:10 +0000

    added wrapper layout component et al

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
 
 export default function Layout({ title, children }) {
@@ -8,10 +9,22 @@ export default function Layout({ title, children }) {
         <title>{title ? title + '- Kerrys Cakes' : 'Kerrys Cakes'}</title>
         <meta name="description" content="Cake Ecommerce website" />
       </Head>
-      <div>
-        <header>header</header>
-        <main>{children}</main>
-        <footer>footer</footer>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 justify-between px-4 shadow-md items-center">
+            <Link href="/">
+              <span className="text-lg font-semibold">Kerrys Cakes</span>
+            </Link>
+            <div className="space-x-4">
+              <Link href="/cart">Cart</Link>
+              <Link href="/login">Login</Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          Kerrys Cakes @ 2022
+        </footer>
       </div>
     </>
   );
