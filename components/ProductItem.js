ae836f09e55a84bfe72ffe8fd9bb3422602e2455
joshnevard <joshnevard@gmail.com>
2022-12-02 12:47:14 +0000

    create product item component added imgs/logo

---
+++ components/ProductItem.js
@@ -0,0 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import React from 'react';
+
+export default function ProductItem({ product }) {
+  return (
+    <div className="card mt-10">
+      <Link href={`/product/${product.slug}`}>
+        <img
+          src={product.image}
+          alt={product.name}
+          className="rounded shadow w-[300px] h-[300px] object-cover mx-auto"
+        />
+      </Link>
+      <div className="flex flex-col items-center p-[5px] justify-center ">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-lg">{product.name}</h2>
+        </Link>
+        <p>£{product.price}</p>
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ utils/data.js
@@ -0,0 +1,75 @@
+const data = {
+  products: [
+    {
+      name: 'Chocolate Cake',
+      slug: 'chocolate-cake',
+      category: 'Chocolate',
+      image: '/images/chocolate-cake.png',
+      price: 15,
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A irresistible choccie cake',
+    },
+    {
+      name: 'Victoria Sponge',
+      slug: 'victoria-sponge',
+      category: 'Victoria Sponge',
+      image: '/images/vickie-sponge.png',
+      price: 15,
+      rating: 4.6,
+      numReviews: 9,
+      countInStock: 20,
+      description: 'A classic with a mouth watering jammy center',
+    },
+    {
+      name: 'Chocolate Whirl',
+      slug: 'chocolate-whirl',
+      category: 'Chocolate',
+      image: '/images/choc-whirl-cake.png',
+      price: 15,
+      rating: 4.5,
+      numReviews: 9,
+      countInStock: 20,
+      description:
+        'Ozzing all things chocolate with a creamy white chocolate filling, topped with chocolate whirls',
+    },
+    {
+      name: 'Childrens Cake',
+      slug: 'childrens-cake',
+      category: 'Childrens',
+      image: '/images/kids-pink-cake.png',
+      price: 13,
+      rating: 4.7,
+      numReviews: 9,
+      countInStock: 20,
+      description: 'This cake is perfect for any childrens celebration',
+    },
+    {
+      name: 'Christmas Cake',
+      slug: 'chrsitmas-cake',
+      category: 'Chirstmas',
+      image: '/images/christmas-cake.png',
+      price: 16,
+      rating: 4.9,
+      numReviews: 9,
+      countInStock: 20,
+      description:
+        'Start your festive celebrations with this chunky christmas cake made to your liking',
+    },
+    {
+      name: 'Celebration Cake',
+      slug: 'celebration-cake',
+      category: 'Celebration',
+      image: '/images/happybirthday-cake.png',
+      price: 15,
+      rating: 4.6,
+      numReviews: 9,
+      countInStock: 20,
+      description:
+        'Share the Happy Birthday vibes with this cake for all, included custom message',
+    },
+  ],
+};
+
+export default data;
