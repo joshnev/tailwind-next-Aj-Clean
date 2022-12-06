0605efa8b1f55c90fd10aa928a6be56157dd05d5
joshnevard <joshnevard@gmail.com>
2022-12-06 16:00:05 +0000

    change quanitity in shopping cart functionality

--- pages/cart.js
+++ pages/cart.js
@@ -14,6 +14,10 @@ export default function CartScreen() {
   const removeItemHandler = (item) => {
     dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
   };
+  const updateCartHandler = (item, qty) => {
+    const quantity = Number(qty);
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantity } });
+  };
 
   return (
     <Layout title="Shopping Cart">
@@ -55,7 +59,20 @@ export default function CartScreen() {
                         </span>
                       </Link>
                     </td>
-                    <td className="p-5 text-right">{item.quanity}</td>
+                    <td className="p-5 text-right">
+                      <select
+                        value={item.quantity}
+                        onChange={(e) =>
+                          updateCartHandler(item, e.target.value)
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </td>
                     <td className="p-5 text-right">£{item.price}</td>
                     <td className="p-5 text-center">
                       <button
