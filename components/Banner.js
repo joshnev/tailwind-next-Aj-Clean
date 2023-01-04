69a5600e9d64c67d7c7e875edcd8c2efed074d20
joshnevard <joshnevard@gmail.com>
2023-01-04 14:38:44 +0000

    product model implemented with seed

--- components/Banner.js
+++ components/Banner.js
@@ -4,10 +4,10 @@ export default function Banner() {
   return (
     <div className="bg-red-500 text-white lg:max-w-[50%] font-primary text-sm mx-auto min-h-[40px] rounded-[5px] flex flex-row justify-center items-center mb-5 drop-shadow-lg">
       <div className="">
-        <h1 className="font-bold">🎄 Ready for the festive season!?</h1>
+        <h1 className="font-bold">Ready for a New Year carpet clean!? </h1>
       </div>
       &nbsp;
-      <p>Sign up for a 10% discount & free carpet protection 🎄</p>
+      <p>Sign up for a 10% discount & free carpet protection.</p>
     </div>
   );
 }
